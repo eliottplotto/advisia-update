c2c21ba781081aa3e72b64523c8c6a054d3ddfe1
Ferrer-Enzo <enzoferrer.pro@gmail.com>
2025-07-16 09:50:51 +0200

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -9,16 +9,16 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./src/sanity/env";
-import { schemaTypes } from "./src/sanity/schemaTypes";
-import { structure } from "./src/sanity/structure";
+import { apiVersion, dataset, projectId } from "@/sanity/lib/api";
+import { schemaTypes } from "@/sanity/schemaTypes";
+import { structure } from "@/sanity/structure";
 
 export default defineConfig({
   basePath: "/studio",
   projectId,
   dataset,
-  title: 'Advisia',
-  subtitle: 'Administration Advisia',
+  title: "Advisia",
+  subtitle: "Administration Advisia",
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema: {
     types: schemaTypes,
